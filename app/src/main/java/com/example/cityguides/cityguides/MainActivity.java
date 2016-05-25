3cda7a01c53d081b0cd45e1c7f77fd723311a6c9
subh005 <subhajit005@gmail.com>
2016-05-25 18:35:51 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/cityguides/cityguides/MainActivity.java
+++ app/src/main/java/com/example/cityguides/cityguides/MainActivity.java
@@ -15,24 +15,31 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+         //  setting the intial view to activity-main.xml page
         setContentView(R.layout.activity_main);
     }
+    // Shows the details for Recipe 1
     public void showRecipeOneDetails(View view){
         setContentView(R.layout.recipe_one_details);
     }
+     // Shows the details for Recipe 2
     public void showRecipeTwoDetails(View view){
         setContentView(R.layout.recipe_two_details);
     }
-
+ // Shows the details for Recipe 3
     public void showRecipeThreeDetails(View view){
         setContentView(R.layout.recipe_three_details);
     }
+     // Shows the details for Recipe 4
     public void showRecipeFourDetails(View view){
         setContentView(R.layout.recipe_four_details);
     }
+     // Shows the details for Recipe 5
     public void showRecipeFiveDetails(View view){
         setContentView(R.layout.recipe_five_details);
     }
+     
+     // Shows a Toast which contains Recipes , checked by the  user
     public void showMyFavourites(View view) {
 
         CheckBox recipeOne = (CheckBox) findViewById(R.id.recipeOne);
@@ -47,20 +54,32 @@ public class MainActivity extends ActionBarActivity {
         boolean hasRecipeFiveChecked = recipeFive.isChecked();
         Toast.makeText(view.getContext(), "" + displayMessage(hasRecipeOneChecked, hasRecipeTwoChecked,hasRecipeThreeChecked,hasRecipeFourChecked,hasRecipeFiveChecked )  , Toast.LENGTH_SHORT).show();
     }
+    //  Toast  contains are populated by this method
+    
     public String displayMessage(  boolean hasRecipeOneChecked, boolean hasRecipeTwoChecked, boolean hasRecipeThreeChecked,boolean hasRecipeFourChecked,boolean hasRecipeFiveChecked ){
         String summaryMessage = "My Favourite : ";
+        //  checks if Recipe 1 was checked
+        // If yes, added to the summary message
         if(hasRecipeOneChecked){
             summaryMessage = summaryMessage +"\n Dal Ka Paratha";
         }
+         //  checks if Recipe 2 was checked
+        // If yes, added to the summary message
         if(hasRecipeTwoChecked){
             summaryMessage = summaryMessage +"\n Moong Dal Cheela";
         }
+        // //  checks if Recipe 3 was checked
+        // If yes, added to the summary message
         if(hasRecipeThreeChecked){
             summaryMessage = summaryMessage +"\n Rawa Upma";
         }
+         //  checks if Recipe 4 was checked
+        // If yes, added to the summary message
         if(hasRecipeFourChecked){
             summaryMessage = summaryMessage +"\n Uttapam";
         }
+         //  checks if Recipe 5 was checked
+        // If yes, added to the summary message
         if(hasRecipeFiveChecked){
             summaryMessage = summaryMessage +"\n Poha";
         }
